05a3f912d5289c8c2093e88df5877ddc31e3d337
voytekkrol <wojtekmkrol@gmail.com>
2020-12-13 23:43:09 +0100

    Added customer and admin areas

--- Uplift/Startup.cs
+++ Uplift/Startup.cs
@@ -62,7 +62,7 @@ namespace Uplift
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
         }
