3a7cf3389b67a6169e1ec197aaf72f0f6eb4c223
voytekkrol <wojtekmkrol@gmail.com>
2020-12-15 10:48:48 +0100

    Added parts of repository pattern

---
+++ Uplift.DataAccess/Data/Repository/Repository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
