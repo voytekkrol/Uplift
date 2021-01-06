0912927acd85d4acae379e24fc8639a010a61162
voytekkrol <wojtekmkrol@gmail.com>
2021-01-06 11:21:21 +0100

    Added service to repository , create service controller and serviceVM

---
+++ Uplift.Models/ViewModels/ServiceVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Uplift.Models.ViewModels
+{
+    public class ServiceVM
+    {
+        public Service Service { get; set; }
+
+        public IEnumerable<SelectListItem> MyProperty { get; set; }
+    }
+}
---
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository.IRepository;
+
+namespace Uplift.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServiceController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnviroment;
+
+        public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _hostEnviroment = hostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
