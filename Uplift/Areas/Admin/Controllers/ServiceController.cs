30208fc72cb998a2203ab94a1d7158d6822c3170
voytekkrol <wojtekmkrol@gmail.com>
2021-01-08 18:48:20 +0100

    Added Index and Upsert views

--- Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models.ViewModels;
 
 namespace Uplift.Areas.Admin.Controllers
 {
@@ -24,11 +25,28 @@ namespace Uplift.Areas.Admin.Controllers
             return View();
         }
 
+        public IActionResult Upsert(int? id)
+        {
+            ServiceVM ServVm = new ServiceVM()
+            {
+                Service = new Models.Service(),
+                CategoryList = _unitOfWork.Category.GetCategoryListFromDropdown(),
+                FrequencyList = _unitOfWork.Frequency.GetFrequencyListFromDropdown()
+            };
+
+            if (id != null)
+            {
+                ServVm.Service = _unitOfWork.Service.Get(id.GetValueOrDefault());
+            }
+
+            return View(ServVm);
+        }
+
         #region API Calls
 
         public IActionResult GetAll()
         {
-            return Json(new { data = _unitOfWork.Service.GetAll() });
+            return Json(new { data = _unitOfWork.Service.GetAll(includeProperties:"Category,Frequency") });
         }
         #endregion
     }
