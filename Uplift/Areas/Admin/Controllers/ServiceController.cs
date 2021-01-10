aa48ef87716755cb32c6b461dab0036645594383
voytekkrol <wojtekmkrol@gmail.com>
2021-01-10 22:23:56 +0100

    Added upsert post method to service controller

--- Uplift/Areas/Admin/Controllers/ServiceController.cs
+++ Uplift/Areas/Admin/Controllers/ServiceController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -15,6 +16,9 @@ namespace Uplift.Areas.Admin.Controllers
         private readonly IUnitOfWork _unitOfWork;
         private readonly IWebHostEnvironment _hostEnviroment;
 
+        [BindProperty]
+        public ServiceVM ServVM { get; set; }
+
         public ServiceController(IUnitOfWork unitOfWork, IWebHostEnvironment hostEnvironment)
         {
             _unitOfWork = unitOfWork;
@@ -27,7 +31,7 @@ namespace Uplift.Areas.Admin.Controllers
 
         public IActionResult Upsert(int? id)
         {
-            ServiceVM ServVm = new ServiceVM()
+            ServVM = new ServiceVM()
             {
                 Service = new Models.Service(),
                 CategoryList = _unitOfWork.Category.GetCategoryListFromDropdown(),
@@ -36,10 +40,65 @@ namespace Uplift.Areas.Admin.Controllers
 
             if (id != null)
             {
-                ServVm.Service = _unitOfWork.Service.Get(id.GetValueOrDefault());
+                ServVM.Service = _unitOfWork.Service.Get(id.GetValueOrDefault());
             }
 
-            return View(ServVm);
+            return View(ServVM);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert() {
+            if (ModelState.IsValid)
+            {
+                string webRootPath = _hostEnviroment.WebRootPath;
+                var files = HttpContext.Request.Form.Files;
+                if (ServVM.Service.Id == 0)
+                {
+                    //New Service
+                    string fileName = Guid.NewGuid().ToString();
+                    var uploads = Path.Combine(webRootPath, @"images\services");
+                    var extension = Path.GetExtension(files[0].FileName);
+
+                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStreams);
+                    }
+                    ServVM.Service.ImageUrl = @"images\services\" + fileName + extension;
+
+                    _unitOfWork.Service.Add(ServVM.Service);
+                }
+                else
+                {
+                    //Edit Service
+                    var serviceFromDb = _unitOfWork.Service.Get(ServVM.Service.Id);
+                    if (files.Count > 0)
+                    {
+                        string fileName = Guid.NewGuid().ToString();
+                        var uploads = Path.Combine(webRootPath, @"images\services");
+                        var extension_new = Path.GetExtension(files[0].FileName);
+
+                        var imagePath = Path.Combine(webRootPath, serviceFromDb.ImageUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(imagePath))
+                        {
+                            System.IO.File.Delete(imagePath);
+                        }
+
+                        using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension_new), FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStreams);
+                        }
+                        ServVM.Service.ImageUrl = @"images\services\" + fileName + extension_new;
+                    }
+                    else
+                    {
+                        ServVM.Service.ImageUrl = serviceFromDb.ImageUrl;
+                    }
+
+                    _unitOfWork.Service.Update(ServVM.Service);
+                }
+            }
         }
 
         #region API Calls
