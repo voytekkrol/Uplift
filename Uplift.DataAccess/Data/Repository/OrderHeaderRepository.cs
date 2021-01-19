4b870d48c3343bc9127ae80ce4df0f649cd7834a
voytekkrol <wojtekmkrol@gmail.com>
2021-01-19 21:50:35 +0100

    Add OrderHeader and OrderDetails to DataAccess

---
+++ Uplift.DataAccess/Data/Repository/OrderHeaderRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Uplift.DataAccess.Data.Repository.IRepository;
+using Uplift.Models;
+
+namespace Uplift.DataAccess.Data.Repository
+{
+    public class OrderHeaderRepository : Repository<OrderHeader>, IOrderHeaderRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public OrderHeaderRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+    }
+}
