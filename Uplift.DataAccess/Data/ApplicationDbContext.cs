84bfd58b3cfc2f32e12fa86abe5cc3ff64a32d3e
voytekkrol <wojtekmkrol@gmail.com>
2021-01-23 23:56:52 +0100

    Added OrderHeader and OrderDetails to Database created CartController

--- Uplift.DataAccess/Data/ApplicationDbContext.cs
+++ Uplift.DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,7 @@ namespace Uplift.DataAccess.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<Frequency> Frequency { get; set; }
         public DbSet<Service> Service { get; set; }
+        public DbSet<OrderHeader> OrderHeader { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
     }
 }
---
+++ Uplift/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Uplift.DataAccess.Data.Repository;
+
+namespace Uplift.Areas.Customer.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public CartController()
+        {
+
+        }
+
+        [Area("Customer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
